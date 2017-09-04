b306a332ff4798d503c33a824d6e5060e88945f6
Kurt Blackwell <kurt@blackwell.id.au>
2017-09-04 17:00:39 +0800

    Add HD stream cookie hack

--- app/src/main/java/au/id/blackwell/kurt/lantv/MainActivity.java
+++ app/src/main/java/au/id/blackwell/kurt/lantv/MainActivity.java
@@ -3,6 +3,7 @@ package au.id.blackwell.kurt.lantv;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.CookieManager;
 import android.webkit.WebView;
 
 public final class MainActivity extends AppCompatActivity {
@@ -27,9 +28,14 @@ public final class MainActivity extends AppCompatActivity {
             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
+        // If we set this cookie then it'll allow us to use the HD page.
+        // I'm not sure if the quality is actually any better.
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setCookie("http://tv.cctv.com/", "country_code=CN; path=/live/cctv13");
+
         //MediaDetails media = new MediaDetails(Uri.fromFile((new File("/storage/sdcard/Download/big_buck_bunny.mp4"))));
         //IMediaResolver resolver = new StaticMediaResolver(media);
-        IMediaResolver resolver = new CctvMediaResolver(mWebViewPool, "http://tv.cctv.com/live/cctv13/sd/index.shtml");
+        IMediaResolver resolver = new CctvMediaResolver(mWebViewPool, "http://tv.cctv.com/live/cctv13/hd/index.shtml");
         mVideo.play(resolver);
     }
 
